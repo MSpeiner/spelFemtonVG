306266431d3ddbca4f28285ae24046610df4f711
Filip Detterfelt <fillip.detterfelt@gmail.com>
2023-10-31 14:50:30 +0100

    Skapar upp knappar i klassen metoder

--- src/Metoder.java
+++ src/Metoder.java
@@ -6,4 +6,22 @@ public class Metoder extends JFrame{
     JPanel northPanel;
     JPanel centerPanel;
     JPanel southPanel;
+
+    //skapar upp 16 knappar för centerPanel
+    JButton knappEtt = new JButton("1");
+    JButton knappTvå = new JButton("2");
+    JButton knappTre = new JButton("3");
+    JButton knappFyra = new JButton("4");
+    JButton knappFem = new JButton("5");
+    JButton knappSex = new JButton("6");
+    JButton knappSju = new JButton("7");
+    JButton knappÅtta = new JButton("8");
+    JButton knappNio = new JButton("9");
+    JButton knappTio = new JButton("10");
+    JButton knappElva = new JButton("11");
+    JButton knappTolv = new JButton("12");
+    JButton knappTretton = new JButton("13");
+    JButton knappFjorton = new JButton("14");
+    JButton knappFemton = new JButton("15");
+    JButton knappSexton = new JButton(" ");
 }
