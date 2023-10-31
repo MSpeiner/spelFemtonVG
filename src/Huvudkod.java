551ba460e8d8531a46ddfafc794a92be445e03ac
Michael Speiner <speiner.m@gmail.com>
2023-10-31 14:48:50 +0100

    Lägger till paneler till huvudkod

--- src/Huvudkod.java
+++ src/Huvudkod.java
@@ -1,2 +1,9 @@
-public class Huvudkod {
+import javax.swing.*;
+
+public class Huvudkod extends JFrame {
+    //Skapar upp paneler Huvudkod
+    final static JPanel jp = new JPanel();
+    final static JPanel northPanel = new JPanel();
+    final static JPanel centerPanel = new JPanel();
+    final static JPanel southPanel = new JPanel();
 }
