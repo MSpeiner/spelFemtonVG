2d1b28849879cab52390e9d912e1f386eabfea11
Filip Detterfelt <fillip.detterfelt@gmail.com>
2023-10-31 16:00:08 +0100

    Implements ActionListner, skapar upp listor, label,panel samt actionperformed

--- src/ActionListner.java
+++ src/ActionListner.java
@@ -1,3 +1,18 @@
-public class ActionListner {
-    //test
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+//Klassen Actionlistener implementerar ifrån AtctionListener
+public class ActionListner implements ActionListener {
+    //Skapar upp 2 Arraylist av typen JButton, samt 1 label och 1 Jpanel
+    ArrayList<JButton> rättadKnappLista;
+    ArrayList<JButton> knappLista;
+    JLabel vinstMeddelande;
+    JPanel southPanel;
+//Skapar en Actionperformed
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
